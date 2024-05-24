0a140326089d7a6ebb5dd0a9a14657aaf28f8dcc
seanox <service@seanox.de>
2024-05-24 21:10:27 +0200

    #0000 Content: Implementation

---
+++ src/main/java/com/seanox/xmex/content/Configurer.java
@@ -0,0 +1,22 @@
+package com.seanox.xmex.content;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+class Configurer implements WebMvcConfigurer {
+
+    // Handle static content in Spring Boot
+    // https://docs.spring.io/spring-framework/reference/web/webmvc/mvc-config/static-resources.html
+    // https://www.baeldung.com/spring-mvc-static-resources
+
+    // Use default pages in directories
+    // https://stackoverflow.com/questions/28437314/spring-boot-doesnt-map-folder-requests-to-index-html-files/59052438#59052438
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+    }
+}
