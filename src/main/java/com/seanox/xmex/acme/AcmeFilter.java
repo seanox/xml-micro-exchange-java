c480c56310c83b645546b69315148a8eb6deb811
seanox <service@seanox.de>
2024-05-28 19:09:16 +0200

    #0000 Implementation

--- src/main/java/com/seanox/xmex/acme/AcmeFilter.java
+++ src/main/java/com/seanox/xmex/acme/AcmeFilter.java
@@ -1,28 +1,61 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der Apache License.
+ *
+ * XML Micro Exchange
+ * Copyright (C) 2024 Seanox Software Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.seanox.xmex.acme;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @Order(2)
-public class AcmeFilter extends HttpFilter {
+class AcmeFilter extends HttpFilter {
 
-    @Override
-    public void init(final FilterConfig filterConfig)
-            throws ServletException {
-    }
+    @Autowired
+    private AcmeService acmeService;
+
+    // The ACME challenge works for HTTP requests. If HTTPS is used, the ACME
+    // challenge is ignored. It simply does not make sense.
 
     @Override
     protected void doFilter(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
             throws ServletException, IOException {
+        final String requestUri = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+        if (request.isSecure()
+                && requestUri.equals(acmeService.getAcmeTokenUri())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            final PrintWriter responseWriter = response.getWriter();
+            responseWriter.write(acmeService.getAcmeHash());
+            responseWriter.flush();
+            return;
+        }
         chain.doFilter(request, response);
     }
 }
---
+++ src/main/java/com/seanox/xmex/acme/AcmeService.java
@@ -0,0 +1,36 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der Apache License.
+ *
+ * XML Micro Exchange
+ * Copyright (C) 2024 Seanox Software Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.seanox.xmex.acme;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Getter
+@Service
+class AcmeService {
+
+    @Value("${acme.hash}")
+    private String acmeHash;
+
+    @Value("${acme.token.uri}")
+    private String acmeTokenUri;
+}
