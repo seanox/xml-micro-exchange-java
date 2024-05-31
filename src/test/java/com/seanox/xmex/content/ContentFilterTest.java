a54e3717af4c1740ce458813c523053dac52a2dc
seanox <service@seanox.de>
2024-05-31 07:18:14 +0200

    #0000 Implementation

--- src/test/java/com/seanox/xmex/content/ContentFilterTest.java
+++ src/test/java/com/seanox/xmex/content/ContentFilterTest.java
@@ -36,11 +36,109 @@ class ContentFilterTest {
     @Test
     void testGetDirectory_02()
             throws Exception {
-        for (final String path : new String[] {"/", "/x/", "/x/xx/", "/x/xx/xxx/"}) {
+        for (final String path : new String[] {"/", "/x/", "/x/xx/"}) {
             final RequestBuilder requestBuilder = MockMvcRequestBuilders
                     .request(HttpMethod.GET, URI.create(path));
             this.mockMvc.perform(requestBuilder)
                     .andExpect(MockMvcResultMatchers.status().isOk());
+        }
+    }
+
+    @Test
+    void testGetDirectory_03()
+            throws Exception {
+        for (final String path : new String[] {"/x/xx/xxx/"}) {
+            final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .request(HttpMethod.GET, URI.create(path));
+            this.mockMvc.perform(requestBuilder)
+                    .andExpect(MockMvcResultMatchers.status().isNotFound());
+        }
+    }
+
+    @Test
+    void testGetDirectoryDefault_01()
+            throws Exception {
+        final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .request(HttpMethod.GET, URI.create("/"));
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("index.html"));
+    }
+
+    @Test
+    void testGetDirectoryDefault_02()
+            throws Exception {
+        final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .request(HttpMethod.GET, URI.create("/x/"));
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("index.htm"));
+    }
+
+    @Test
+    void testGetDirectoryDefault_03()
+            throws Exception {
+        final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .request(HttpMethod.GET, URI.create("/x/xx/"));
+        this.mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("index.xhtml"));
+    }
+
+    @Test
+    void testHeadDirectory_01()
+            throws Exception {
+        for (final String path : new String[] {"/", "/x/", "/x/xx/"}) {
+            final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .request(HttpMethod.HEAD, URI.create(path));
+            this.mockMvc.perform(requestBuilder)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().string(""));
+        }
+    }
+
+    @Test
+    void testOptionsDirectory_01()
+            throws Exception {
+        for (final String path : new String[] {"/", "/x/", "/x/xx/"}) {
+            final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .request(HttpMethod.OPTIONS, URI.create(path));
+            this.mockMvc.perform(requestBuilder)
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.content().string(""));
+        }
+    }
+
+    @Test
+    void testPostDirectory_01()
+            throws Exception {
+        for (final String path : new String[] {"/", "/x/", "/x/xx/"}) {
+            final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .request(HttpMethod.POST, URI.create(path));
+            this.mockMvc.perform(requestBuilder)
+                    .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+        }
+    }
+
+    @Test
+    void testPutDirectory_01()
+            throws Exception {
+        for (final String path : new String[] {"/", "/x/", "/x/xx/"}) {
+            final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .request(HttpMethod.PUT, URI.create(path));
+            this.mockMvc.perform(requestBuilder)
+                    .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
+        }
+    }
+
+    @Test
+    void testDeleteDirectory_01()
+            throws Exception {
+        for (final String path : new String[] {"/", "/x/", "/x/xx/"}) {
+            final RequestBuilder requestBuilder = MockMvcRequestBuilders
+                    .request(HttpMethod.DELETE, URI.create(path));
+            this.mockMvc.perform(requestBuilder)
+                    .andExpect(MockMvcResultMatchers.status().isMethodNotAllowed());
         }
     }
 }
