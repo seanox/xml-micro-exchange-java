d5e08e5e713553c7d45b1ec0f6ab963a81cc37c7
seanox <service@seanox.de>
2024-06-01 15:16:40 +0200

    #0000 Implementation

--- src/main/java/com/seanox/xmex/Application.java
+++ src/main/java/com/seanox/xmex/Application.java
@@ -3,7 +3,7 @@
  * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
  * Diese Software unterliegt der Version 2 der Apache License.
  *
- * XML Micro Exchange
+ * XMEX XML-Micro-Exchang
  * Copyright (C) 2024 Seanox Software Solutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
@@ -23,17 +23,46 @@
  */
 package com.seanox.xmex;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+class Application extends SpringBootServletInitializer {
+
+    // Enable http and https at the same time on Spring Boot
+    // https://mvysny.github.io/spring-boot-enable-http-https/
 
     public static void main(final String... options) {
         final SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.setBannerMode(Banner.Mode.CONSOLE);
         springApplication.run(options);
+    }
+
+    // The additional HTTP connector is only activated for HTTP-01 challenge if
+    // the application is running with HTTPS and an ACME port has been defined.
+
+    @Value("${acme.port:0}")
+    private int acmePort;
+
+    @Bean
+    @ConditionalOnExpression("('${acme.port:}').matches('^\\d+$')"
+            + " && !('${acme.port:}').matches('^0+$')"
+            + " && ('${server.ssl.enabled}').matches('^(on|true)$')")
+    WebServerFactoryCustomizer<TomcatServletWebServerFactory> addAcmeHttpConnectorCustomizer() {
+        return (final TomcatServletWebServerFactory factory) -> {
+            final Connector connector = new Connector();
+            connector.setPort(this.acmePort);
+            factory.addAdditionalTomcatConnectors(connector);
+        };
     }
 }
