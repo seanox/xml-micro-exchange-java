8ecc53568370fba31a4af3cff57d1c26adbe0d6a
seanox <service@seanox.de>
2024-06-02 08:52:17 +0200

    #0000 Implementation

--- src/main/java/com/seanox/xmex/datasource/DatasourceFilter.java
+++ src/main/java/com/seanox/xmex/datasource/DatasourceFilter.java
@@ -1,28 +1,73 @@
+/**
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der Apache License.
+ *
+ * XMEX XML-Micro-ExchangExchange
+ * Copyright (C) 2024 Seanox Software Solutions
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.seanox.xmex.datasource;
 
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @Component
 @Order(1)
-public class DatasourceFilter extends HttpFilter {
+class DatasourceFilter extends HttpFilter {
 
-    @Override
-    public void init(final FilterConfig filterConfig)
-            throws ServletException {
-    }
+    @Autowired
+    private DatasourceService datasourceService;
+
+    @Value("#{new Boolean(('${server.ssl.enabled:}').matches('^(on|true)$'))}")
+    private boolean isSecureConnection;
+
+    // If the HTTPS Connector is used, all HTTP requests are redirected to HTTPS.
 
     @Override
     protected void doFilter(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
             throws ServletException, IOException {
-        chain.doFilter(request, response);
+        if (!request.isSecure()
+                && isSecureConnection) {
+            final StringBuilder requestUrl = new StringBuilder(request.getRequestURL().toString());
+            final String queryString = request.getQueryString();
+            if (Objects.nonNull(queryString)
+                    && !queryString.isBlank())
+                requestUrl.append("?").append(queryString);
+            response.sendRedirect(requestUrl.toString()
+                    .replaceAll("^(?i)(http)(.//)", "$1s$2"));
+            return;
+        }
+
+        final String requestUri = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+        if (!requestUri.startsWith(this.datasourceService.getServiceUri())) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        response.flushBuffer();
     }
 }
