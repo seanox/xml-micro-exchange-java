5904b5e8deed8906b6c95b7889bafddf17550d9a
seanox <service@seanox.de>
2024-06-04 06:22:41 +0200

    #0000 Implementation

--- src/main/java/com/seanox/xmex/inbound/InboundFilter.java
+++ src/main/java/com/seanox/xmex/inbound/InboundFilter.java
@@ -20,21 +20,25 @@
  */
 package com.seanox.xmex.inbound;
 
+import com.seanox.xmex.Application;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.apachecommons.CommonsLog;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
+import java.util.UUID;
 
+
+@CommonsLog
 @Component
 @Order(1)
 class InboundFilter extends HttpFilter {
@@ -46,27 +50,37 @@ class InboundFilter extends HttpFilter {
     private String acmeTokenUri;
 
     @Override
-    protected void doFilter(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
-            throws ServletException, IOException {
+    protected void doFilter(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) {
+        try {
+            final String requestUri = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
+            final String queryString = request.getQueryString();
 
-        final String requestUri = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
-        final String queryString = request.getQueryString();
-
-        // If the HTTPS connector is used, all HTTP requests, with exception of
-        // ACME requests, are redirected to HTTPS.
-        if (!request.isSecure()
-                && isSecureConnection
-                && !requestUri.equals(acmeTokenUri)) {
-            final StringBuilder requestUrl = new StringBuilder(request.getRequestURL().toString());
-            if (Objects.nonNull(queryString)
-                    && !queryString.isBlank())
-                requestUrl.append("?").append(queryString);
-            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-            response.setHeader(HttpHeaders.LOCATION, requestUrl.toString()
-                    .replaceAll("^(?i)(http)(://)", "$1s$2"));
-            return;
+            // If the HTTPS connector is used, all HTTP requests, with exception of
+            // ACME requests, are redirected to HTTPS.
+            if (!request.isSecure()
+                    && isSecureConnection
+                    && !requestUri.equals(acmeTokenUri)) {
+                final StringBuilder requestUrl = new StringBuilder(request.getRequestURL().toString());
+                if (Objects.nonNull(queryString)
+                        && !queryString.isBlank())
+                    requestUrl.append("?").append(queryString);
+                response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+                response.setHeader(HttpHeaders.LOCATION, requestUrl.toString()
+                        .replaceAll("^(?i)(http)(://)", "$1s$2"));
+                return;
+            }
+            chain.doFilter(request, response);
+        } catch (Throwable throwable) {
+            final String uniqueTime = Long.toString(Math.abs(System.currentTimeMillis()), 36);
+            final String uniqueHash = Long.toString(Math.abs(uniqueTime.hashCode()), 36);
+            final String unique = new StringBuilder()
+                    .append(Long.toString(uniqueHash.length(), 36))
+                    .append(uniqueHash)
+                    .append(uniqueTime)
+                    .toString();
+            LogFactory.getLog(Application.class)
+                    .error(String.format("Unexpected error occurred: %s", unique), throwable);
+            // TODO: Response modification (500, ...)
         }
-
-        chain.doFilter(request, response);
     }
 }
